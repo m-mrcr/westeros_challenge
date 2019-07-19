3c54071cd782424912453765883ea96b4b17a928
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 11:50:43 -0600

    passing test

--- spec/features/search/find_all_members_spec.rb
+++ spec/features/search/find_all_members_spec.rb
@@ -14,21 +14,21 @@ feature 'search index' do
         select "Greyjoy", from: "house"
 
         # And I click on "Get Members"
-        find('input[value="Get Memebers"]').click
+        find('input[value="Get Members"]').click
 
         # Then my path should be "/search" with "house=greyjoy" in the parameters
-        expect(current_path).to eq("/search?house=greyjoy")
+        expect(current_path).to eq("/search")
 
         # And I should see a message "7 Members"
         expect(page).to have_content("7 Members")
 
         # And I should see a list of the 7 members of House Greyjoy
-        expect(page).to have_css(".member", 7)
+        expect(page).to have_css(".member", count: 7)
 
         # And I should see a name and id for each member.
         within(first(".member")) do
-          expect(page).to have_content("Name: ")
-          expect(page).to have_content("ID: ")
+          expect(page).to have_content("Member Name: ")
+          expect(page).to have_content("Member ID: ")
         end
     end
   end
