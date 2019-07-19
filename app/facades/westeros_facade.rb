2eca93bf9e0e551864d200157e90e5cc84bb2479
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 09:52:10 -0600

    service, not facade

--- app/facades/westeros_facade.rb
+++ app/facades/westeros_facade.rb
@@ -1,21 +1,3 @@
 class WesterosFacade
 
-  def get_members(house)
-    get_json("house/#{house}")
-  end
-
-  private
-
-  def get_json(url, params = nil)
-    response = conn.get(url, params)
-    JSON.parse(response.body, symbolize_names: true)
-  end
-
-  def conn
-    Faraday.new(url: 'http://westerosapi.herokuapp.com/api/v1') do |f|
-      f.headers['api_key'] = ENV['WESTEROS_API_KEY']
-      f.adapter Faraday.default_adapter
-    end
-
 end
-http://westerosapi.herokuapp.com/api/v1/house/stark?api_key=YOUR_KEY_HERE
