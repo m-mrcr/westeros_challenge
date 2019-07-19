5d0a52328bc340dad949c00808198126f803f156
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-07-19 09:51:22 -0600

    getting the service up and running

---
+++ spec/features/search/find_all_members_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+feature 'search index' do
+  # As a user
+  describe 'as a user' do
+    scenario 'I search for members by houses' do
+      # VCR.use_cassette('visitor_search_by_city') do
+        WebMock.disable!
+
+        # When I visit "/"
+        visit root_path
+
+        # And I select "Greyjoy" from the dropdown
+        find('#familySelect').find(:xpath, 'Greyjoy').select_option
+
+        # And I click on "Get Members"
+        click_link "Get Members"
+
+        # Then my path should be "/search" with "house=greyjoy" in the parameters
+        expect(current_path).to eq("/search?house=greyjoy")
+
+        # And I should see a message "7 Members"
+        expect(page).to have_content("7 Members")
+
+        # And I should see a list of the 7 members of House Greyjoy
+        expect(page).to have_css(".member", 7)
+
+        # And I should see a name and id for each member.
+        within(first(".member")) do
+          expect(page).to have_content("Name: ")
+          expect(page).to have_content("ID: ")
+        end
+
+      # end
+    end
+  end
+end
---
+++ spec/services/westeros_service_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+describe WesterosService do
+  it "finds current house's members" do
+
+    something = WesterosService.new
+    binding.pry
+
+  end
+end
